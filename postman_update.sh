0527a74df1e675261a227f6c00f1c02cb92d6242
Ludim <ludim.anel@gmail.com>
2018-06-15 01:38:57 -0500

    Feat [postman_update.sh]: Update postman

--- postman_update.sh
+++ postman_update.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 ##### Description: Update Native Postman on Ubuntu
-#### Written by: Ludim Sanchez - ludim.anel@gmail.com on Jun-2018
+#### Written by: Ludim Salo on Jun-2018
+#### Inspired: https://calliditasblog.wordpress.com/author/calliditasblog/
+####           https://gist.github.com/aviskase/e642248c35e400b56e2489430952369f
 
-wget -o postman-linux.tar.gz https://dl.pstmn.io/download/latest/linux64
+POSTMAN_FOLDER='postman-linux.tar.gz'
+
+if [ ! -d "Postman" ]; then
+    echo "Download Postman"
+    wget -O $POSTMAN_FOLDER https://dl.pstmn.io/download/latest/linux64
+fi
+
+if [ -f $POSTMAN_FOLDER ]; then
+    echo "Extract Postman"
+    tar -xzf $POSTMAN_FOLDER
+    rm $POSTMAN_FOLDER
+fi
+
+if [ -d "/opt/Postman" ]; then
+    echo "Installing to opt"
+    sudo rm -rf /opt/Postman
+    sudo mv Postman /opt/
+fi
+
