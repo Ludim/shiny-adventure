d834d560910447754a9ec1f80797969ef72c41f9
Ludim <ludim.anel@gmail.com>
2018-06-15 00:17:15 -0500

    Feat [postman_update.sh]: Download latest postman version

---
+++ postman_update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+##### Description: Update Native Postman on Ubuntu
+#### Written by: Ludim Sanchez - ludim.anel@gmail.com on Jun-2018
+
+wget -o postman-linux.tar.gz https://dl.pstmn.io/download/latest/linux64
